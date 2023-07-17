ba16cf9046a2efeb41c5279a4bc50205dd9c372a
Lucas Santos <lucas22.ferreira.007@gmail.com>
2023-07-17 20:24:32 -0300

    feito os exercícios IV e V

--- index.js
+++ index.js
@@ -12,6 +12,19 @@ document.write('Soma de Valores: ' + valor1 + ' + ' + valor2 + ' é igual a ' + 
 let valorCompra = 149.90
 let quantParcelas = 2
 let total = valorCompra / quantParcelas
-document.write('O valor da compra foi de R$' + valorCompra + '.<br>' + 'Forma de pagamento: ' + quantParcelas + 'x ' + 'de R$' + total + '.')
+document.write('O valor da compra foi de R$' + valorCompra + '.<br>' + 'Forma de pagamento: ' + quantParcelas + 'x ' + 'de R$' + total + '.<br><br>')
 
 // Exercício IV
+let minutos = 120
+let segundos = 120 * 60
+document.write(`${minutos} minutos equivale à ${segundos} segundos. <br><br>`)
+
+// Exercício V
+let aluno = 'Lucas Ferreira'
+let nota1 = 9
+let nota2 = 7
+let nota3 = 10
+let media = (nota1 + nota2 + nota3) / 3
+document.write(`O aluno ${aluno} ficou com a média de ${media}.<br><br>`)
+
+// Exercício VI
