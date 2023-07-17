bea3fe11bc2c5f253a19b299a46121d51bdea702
Lucas Santos <lucas22.ferreira.007@gmail.com>
2023-07-17 20:05:53 -0300

    feito os exercícios I, II e III

---
+++ index.js
@@ -0,0 +1,17 @@
+// Exercício I
+let idade = 25
+paragrafo = document.write('A minha idade é ' + idade + ' anos.' + '<br><br>')
+
+// Exercício II
+let valor1 = 20
+let valor2 = 20
+let resultado = valor1 + valor2
+document.write('Soma de Valores: ' + valor1 + ' + ' + valor2 + ' é igual a ' + resultado + '.' + '<br><br>')
+
+// Exercício III
+let valorCompra = 149.90
+let quantParcelas = 2
+let total = valorCompra / quantParcelas
+document.write('O valor da compra foi de R$' + valorCompra + '.<br>' + 'Forma de pagamento: ' + quantParcelas + 'x ' + 'de R$' + total + '.')
+
+// Exercício IV
