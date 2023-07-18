a6b5ed70448b223a599716905e8ef2af72b3483e
Lucas Santos <lucas22.ferreira.007@gmail.com>
2023-07-17 21:15:25 -0300

    feito os exercícios VI, VII e VIII

--- index.js
+++ index.js
@@ -25,6 +25,40 @@ let nota1 = 9
 let nota2 = 7
 let nota3 = 10
 let media = (nota1 + nota2 + nota3) / 3
-document.write(`O aluno ${aluno} ficou com a média de ${media}.<br><br>`)
+document.write(`O aluno ${aluno} ficou com a média de ${media.toFixed(2)}.<br><br>`)
 
 // Exercício VI
+let a = 10
+let b = 20
+a = 20
+b = 10
+document.write(`O valor da variável A é de ${a} e da B é de ${b}.<br><br>`)
+
+// Exercício VII
+let eleitores = 12230
+let votosBrancos = 49
+let votosNulos = 124
+let votosValidos = eleitores - (votosBrancos + votosNulos)
+let percentualBrancos = (votosBrancos / eleitores) * 100
+let percentualNulos = (votosNulos / eleitores) * 100
+let percentualValidos = (votosValidos / eleitores) * 100
+document.write(`O município possui um total de ${eleitores} eleitores. Após a apuração das urnas, ${votosBrancos} eleitores votaram em branco, que corresponde a ${percentualBrancos.toFixed(2)}%. ${votosNulos} eleitores votaram nulo, que corresponde a ${percentualNulos.toFixed(2)}%. Por fim, ${votosValidos} eleitores tiveram seus votos validados, que corresponde a ${percentualValidos.toFixed(2)}%.<br><br>`)
+
+// Exercício VIII
+let numero1 = 10
+let numero2 = 12
+    if(numero1 === numero2){
+        document.write(`Números iguais.<br><br>`)
+    } else if(numero1 > numero2) {
+        document.write(`Primeiro é maior.<br><br>`)
+    } else {
+        document.write(`O segundo é maior.<br><br>`)
+    }
+
+// Exercício IX
+
+// Exercício X
+
+// Exercício XI
+
+// Exercício XII
